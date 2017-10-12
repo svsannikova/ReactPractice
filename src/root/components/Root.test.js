ecc2a8c544c92fc5346cf900843e59aec3aa33df
svsannik <svetlana.sannikova@digia.com>
2017-10-12 10:51:04 +0300

    Revert "initial comment"
    This reverts commit 08a2bb08eddd97ec5655b8751a2308d4c197b885.

--- src/root/components/Root.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {render} from 'react-dom'
-import Root from './Root'
-
-test('Root component renders without crashing', () => {
-  const div = document.createElement('div')
-  render(<Root />, div)
-})
