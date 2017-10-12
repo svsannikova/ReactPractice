256612fb2ea3020b1f082d5203aa4d108fd6ae0a
svsannik <svetlana.sannikova@digia.com>
2017-10-12 12:01:41 +0300

    initial commit

---
+++ src/actions/createNewPoll.js
@@ -0,0 +1,23 @@
+export function savePoll(text) {
+  const body = {
+    text,
+  }
+  console.log(body)
+  return fetch('http://localhost:3000/polls', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    mode: 'cors',
+    cache: 'default',
+    body: JSON.stringify(body),
+  })
+    .then(r=>{
+      return r.json()
+    }).then(t=>{
+      console.log(t)
+      return t
+    }).catch(err => {
+      console.log(err.body)
+    })
+}
